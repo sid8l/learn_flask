4c8c0641163ef75cbea3db8017f40e72a5e9b326
sid8l <milosida@gmail.com>
2019-09-24 18:14:39 +0300

    add news from python.org

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 
 app = Flask(__name__)
@@ -8,9 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = 'Weather forecast'
+    title = 'News hoho'
     weather = weather_by_city('Minsk,Belarus')
-    return render_template('index.html', page_title=title, weather=weather)
+    news_lists = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_lists=news_lists)
 
 
 if __name__ == "__main__":
