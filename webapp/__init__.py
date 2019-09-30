674d78c85f52f3da5ec097b917fa44376e781dc2
sid8l <milosida@gmail.com>
2019-09-30 22:49:49 +0300

    add config

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,11 +6,12 @@ from webapp.python_org_news import get_python_news
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
     @app.route('/')
     def index():
         title = 'News hoho'
-        weather = weather_by_city('Minsk,Belarus')
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_lists = get_python_news()
         return render_template('index.html', page_title=title, weather=weather, news_lists=news_lists)
     return app
--- webapp/weather.py
+++ webapp/weather.py
@@ -1,10 +1,11 @@
 import requests
+from flask import current_app
 
 
 def weather_by_city(city_name):
-    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    weather_url = current_app.config['WEATHER_URL']
     params = {
-        'key': '7e6d1864055549e38e473413191609',
+        'key': current_app.config['WEATHER_API_KEY'],
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,
