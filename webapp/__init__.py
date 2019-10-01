2f1a41488910e781d9e017931c9bb902dfc7c8d6
sid8l <milosida@gmail.com>
2019-10-01 17:59:12 +0300

    add script for create db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,7 +8,7 @@ from webapp.model import db
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    db.init_app()
+    db.init_app(app)
 
     @app.route('/')
     def index():
--- webapp/model.py
+++ webapp/model.py
@@ -6,9 +6,9 @@ db = SQLAlchemy()
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
     url = db.Column(db.String, nullable=False, unique=True)
-    published = db.Columnt(db.DateTime, nullable=False)
+    published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
