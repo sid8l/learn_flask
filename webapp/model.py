ca3d501f43777efa9e31d771a9fe0568ab6e3485
sid8l <milosida@gmail.com>
2019-10-07 22:18:00 +0300

    add admin page and user verification

--- webapp/model.py
+++ webapp/model.py
@@ -24,10 +24,14 @@ class User(db.Model, UserMixin):
     role = db.Column(db.String(10), index=True)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} id: {}>'.format(self.username, self.id)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
