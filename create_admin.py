c7c900615d51f30ddff6d7edaee730d7b8313400
sid8l <milosida@gmail.com>
2019-10-07 09:19:15 +0300

    add script for create users

---
+++ create_admin.py
@@ -0,0 +1,28 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app
+from webapp.model import db, User
+
+
+app = create_app()
+
+with app.app_context():
+    username = input('Input username: ')
+
+    if User.query.filter(User.username == username).count():
+        print('This username already exists.')
+        sys.exit()
+
+    password1 = getpass('Password: ')
+    password2 = getpass('Password again: ')
+
+    if not password1 == password2:
+        print('passwords not matched')
+        sys.exit()
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password1)
+    db.session.add(new_user)
+    db.session.commit()
+    print(f'Create new user with id {new_user.id}')
