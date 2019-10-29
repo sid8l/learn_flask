4d2706617905022d5d185eaa1c38e6c632d2ee23
Novikov Artem <milosida@gmail.com>
2019-10-29 13:57:27 +0000

    parse news from habr

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -1,12 +1,33 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import locale
+import platform
 
 from bs4 import BeautifulSoup
 
 from webapp.news.parsers.utils import save_news, get_html
 
 
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, 'russian')
+else:
+    locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
+
+def parse_habr_date(date_str):
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    if 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str = date_str.replace('сегодня', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
+
+
 def get_habr_snippets():
-    html = get_html('https://habr.com/en/search/?target_type=posts&order_by=date&q=python&flow=')
+    html = get_html('https://habr.com/ru/search/?target_type=posts&q=python&order_by=date')
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='content-list_posts').findAll('li', class_='content-list__item_post')
@@ -15,7 +36,8 @@ def get_habr_snippets():
             title = news.find('a', class_='post__title_link').text
             url = news.find('a', class_='post__title_link')['href']
             published = news.find('span', class_='post__time').text
-            print(title, url, published)
+            published = parse_habr_date(published)
+            save_news(title, url, published)
             # try:
             #     published = datetime.strptime(published, '%Y-%m-%d')
             # except ValueError:
