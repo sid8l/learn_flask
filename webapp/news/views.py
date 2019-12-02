db57fccfe217b504dee0da611034d3953ed6ef5e
Novikov Artem <milosida@gmail.com>
2019-12-02 16:31:28 +0000

    comments adding to db

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,6 +1,8 @@
-from flask import abort, Blueprint, render_template, current_app
+from flask import abort, Blueprint, render_template, current_app, flash, redirect, request, url_for
+from flask_login import current_user
 
-from webapp.news.models import News
+from webapp import db
+from webapp.news.models import News, Comments
 from webapp.weather import weather_by_city
 from webapp.news.forms import CommentForm
 
@@ -28,4 +30,19 @@ def single_news(news_id):
 
 @blueprint.route('/news/comment', methods=['POST'])
 def add_comment():
-    pass
+    form = CommentForm()
+    if form.validate_on_submit():
+        if News.query.filter(News.id == form.news_id.data).first():
+            comment = Comments(text=form.comment_text.data, news_id=form.news_id.data,
+                               user_id=current_user.id)
+            db.session.add(comment)
+            db.session.commit()
+            flash('Комментарий добавлен')
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в заполнении поля "{}": - {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+    return redirect(request.referrer)
